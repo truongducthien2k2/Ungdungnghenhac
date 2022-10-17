56b866744f980216d5a581d9fb3b689400822fd4
truongducthien2k2 <truongducthien2k2@gmail.com>
2022-10-17 23:55:51 +0700

    ui

--- music/MainWindow.xaml.cs
+++ music/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace music
 {
@@ -23,11 +12,37 @@ namespace music
         public MainWindow()
         {
             InitializeComponent();
+        }     
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }    
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        bool IsMaximize = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            if (e.ClickCount == 2)
+            {
+                if (true)
+                {
+                    if (IsMaximize)
+                    {
+                        this.WindowState = WindowState.Normal;
+                        this.Width = 1280;
+                        this.Height = 780;
 
+                        IsMaximize = false;
+                    }
+                    else
+                    {
+                        this.WindowState = WindowState.Maximized;
+                        IsMaximize=true;
+                    } 
+                }
+            }
         }
     }
 }
