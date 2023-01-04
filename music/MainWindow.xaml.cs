145daa3ab92d37879067fd08e3482869b87c5fc4
Lê Phúc <lethephuc2002@gmail.com>
2023-01-04 23:14:11 +0700

    base-ui-update

--- music/MainWindow.xaml.cs
+++ music/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using music.View;
+using System;
+using System.Windows;
 using System.Windows.Input;
 
 
@@ -8,41 +10,131 @@ namespace music
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+        {
         public MainWindow()
         {
             InitializeComponent();
-        }     
-        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+            navFrame.Navigate(new HomeView());
+        }
+
+        private void Border_MouseDown( object sender, MouseButtonEventArgs e )
         {
-            if(e.ChangedButton == MouseButton.Left)
-            {
+            if ( e.ChangedButton == MouseButton.Left )
+                {
                 this.DragMove();
-            }    
+                }
         }
 
         bool IsMaximize = false;
-        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void Border_MouseLeftButtonDown( object sender, MouseButtonEventArgs e )
         {
-            if (e.ClickCount == 2)
-            {
-                if (true)
+            if ( e.ClickCount == 2 )
                 {
-                    if (IsMaximize)
-                    {
-                        this.WindowState = WindowState.Normal;
-                        this.Width = 1280;
-                        this.Height = 780;
+                    if ( true )
+                        {
+                            if ( IsMaximize )
+                            {
+                                this.WindowState = WindowState.Normal;
+                                this.Width = 1280;
+                                this.Height = 780;
 
-                        IsMaximize = false;
-                    }
-                    else
-                    {
-                        this.WindowState = WindowState.Maximized;
-                        IsMaximize=true;
-                    } 
+                                IsMaximize = false;
+                            }
+                            else
+                            {
+                                this.WindowState = WindowState.Maximized;
+                                IsMaximize = true;
+                            }
+                        }
                 }
             }
+
+        private void minWindowBtn_Click( object sender, RoutedEventArgs e )
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void maxWindowBtn_Click( object sender, RoutedEventArgs e )
+        {
+            if ( this.WindowState == WindowState.Normal )
+                this.WindowState = WindowState.Maximized;
+            else this.WindowState = WindowState.Normal;
+        }
+
+        private void closeWindowBtn_Click( object sender, RoutedEventArgs e )
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void homeBtn_Click( object sender, RoutedEventArgs e )
+        {
+            navFrame.Navigate(new HomeView());
+        }
+
+        private void rankBtn_Click( object sender, RoutedEventArgs e )
+        {
+            navFrame.Navigate(new RankView());
+
+        }
+
+        private void historyBtn_Click( object sender, RoutedEventArgs e )
+        {
+            navFrame.Navigate(new HistoryView());
+
+        }
+
+        private void topicBtn_Click( object sender, RoutedEventArgs e )
+        {
+            navFrame.Navigate(new TopicView());
+
+        }
+
+        private void albumBtn_Click( object sender, RoutedEventArgs e )
+        {
+            navFrame.Navigate(new AlbumView());
+
+        }
+
+        private void artistBtn_Click( object sender, RoutedEventArgs e )
+        {
+            navFrame.Navigate(new ArtistView());
+
+        }
+
+        private void songBtn_Click( object sender, RoutedEventArgs e )
+        {
+            navFrame.Navigate(new SongView());
+
+        }
+
+        private void videoBtn_Click( object sender, RoutedEventArgs e )
+        {
+            navFrame.Navigate(new VideoView());
+
+        }
+
+        private void songInDeviceBtn_Click( object sender, RoutedEventArgs e )
+        {
+            navFrame.Navigate(new SongInDeviceView());
+
+        }
+
+        private void favoriteListBtn_Click( object sender, RoutedEventArgs e )
+        {
+            navFrame.Navigate(new FavoriteListView());
+
+        }
+
+        private void playListBtn_Click( object sender, RoutedEventArgs e )
+        {
+            navFrame.Navigate(new PlayListView());
+
+        }
+
+        private void accountBtn_Click( object sender, RoutedEventArgs e )
+        {
+            navFrame.Navigate(new AccountView());
+
         }
     }
 }
